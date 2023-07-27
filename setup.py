e48fd8eeb62cba0566dadf5dd66a607702433a92
rajandevkota98 <r.devkota.98@gmail.com>
2023-07-27 18:38:52 +0545

    configuration fixed

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
-from setuptools import setup,find_packages
+from setuptools import find_packages, setup
+from typing import List
+
 
 def get_requirements():
     requirement_list = []
     file_path = 'requirements.txt'
     with open(file_path) as file:
         requirement_list = file.readlines()
-        requirement_list = [require.replace('\n', '') for require in requirement_list]
-
-        if '- e.' in requirement_list:
-            requirement_list.remove('- e.')
+        requirement_list = [require.replace('\n','') for require in requirement_list]
+        if '- e. ' in requirement_list:
+            requirement_list.remove('- e. ')
     return requirement_list
-        
 
 
 setup(
     name='xray',
-    version='1.0.0',
+    version='1.0.1',
+    description='Internship Project',
     author='Rajan Devkota',
     author_email='r.devkota.98@gmail.com',
-    package= find_packages(),
-    install_require = get_requirements())
+    packages=find_packages(),
+    install_require = get_requirements(),
+)
