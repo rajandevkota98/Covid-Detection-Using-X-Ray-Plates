c903f3bc09e6b13c5003e1d29ba6e07fdee11840
rajandevkota98 <r.devkota.98@gmail.com>
2023-07-31 18:35:04 +0545

    bug fixed

--- xray/components/model_evaluation.py
+++ xray/components/model_evaluation.py
@@ -30,15 +30,18 @@ class ModelEvaluation:
                     batch_size=self._params_schema['BATCH_SIZE'],
                     class_mode='binary')
             
+            logging.info('Getting Recently Trained Model')
             trained_model_file_path = self.model_trainer_artifact.trained_model_file_path
             model_resolver = ModelResolver()
             is_model_accepted = True
 
             if not model_resolver.is_model_exists():
+                 logging.info('No saved model found')
                  model_evaluation_artifact = ModelEvaluationArtifact(is_model_accepted=is_model_accepted,improved_accuracy= None, best_model_file_path=None, trained_model_file_path=trained_model_file_path)
                  return model_evaluation_artifact
 
             
+            logging.info('Best model found')
             best_model_file_path =model_resolver.SOTA_model()
             best_model = tf.keras.models.load_model(best_model_file_path)
 
@@ -51,6 +54,7 @@ class ModelEvaluation:
             logging.info(f"recent_model_accuracy: {recent_model_accuracy}")
 
             improved_accuracy = recent_model_accuracy - best_model_accuracy
+            logging.info(f'improved_accuracy: {improved_accuracy}')
 
             if self.model_evaluation_config.model_evaluation_threshold < improved_accuracy:
                  is_model_accepted = False
@@ -58,12 +62,12 @@ class ModelEvaluation:
                  is_model_accepted = True
             
             model_evaluation_arifact = ModelEvaluationArtifact(is_model_accepted=is_model_accepted, improved_accuracy=improved_accuracy,trained_model_file_path=trained_model_file_path, best_model_file_path=best_model_file_path)
-
+            logging.info(f'model_evaluation_arifact:{model_evaluation_arifact}')
             model_evalutaion_report = model_evaluation_arifact.__dict__
-
             report_file_path = self.model_evaluation_config.model_evaluation_report_path
             dir_path = os.path.dirname(report_file_path)
             os.makedirs(dir_path, exist_ok=True)
+            logging.info('writing model evaluation report')
             write_yaml_file(self.model_evaluation_config.model_evaluation_report_path,model_evalutaion_report)
             return model_evaluation_arifact
 
