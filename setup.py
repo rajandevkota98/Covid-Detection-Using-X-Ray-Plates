25ef25ac3cd575ab35961f1867f73fa5b36375e2
rajandevkota98 <r.devkota.98@gmail.com>
2023-08-02 15:40:02 +0545

    fixed issue

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ def get_requirements():
     with open(file_path) as file:
         requirement_list = file.readlines()
         requirement_list = [require.replace('\n','') for require in requirement_list]
-        if '- e. ' in requirement_list:
-            requirement_list.remove('- e. ')
+        if "-e ." in requirement_list:
+            requirement_list.remove("-e .")
     return requirement_list
 
 
@@ -20,5 +20,5 @@ setup(
     author='Rajan Devkota',
     author_email='r.devkota.98@gmail.com',
     packages=find_packages(),
-    install_require = get_requirements(),
+    install_requires = get_requirements(),
 )
